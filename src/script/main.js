954edef87a746bafe83d955d5cffca634527009d
Leovison <cleovison@gmail.com>
2023-11-25 14:19:25 +0800

    Added a Modal Preview and Files

--- src/script/main.js
+++ src/script/main.js
@@ -3,6 +3,41 @@ const navigation = document.querySelector(".navbar");
 const navigationHeight = navigation.offsetHeight;
 
 document.documentElement.style.setProperty(
-  "--scroll-padding", 
+  "--scroll-padding",
   navigationHeight + "px"
 );
+
+//Modal
+const openImg = document.querySelector(".open-img");
+const images = document.querySelectorAll(".images > img");
+const hiddenImgs = document.querySelector(".hidden-img");
+const mainImage = document.querySelector(".open-img > img");
+const modalTitle = document.querySelector(".modal-title");
+
+let currentValue = -1;
+
+images.forEach((img, value) => {
+  img.addEventListener("click", (x) => {
+    openImg.classList.toggle("active");
+
+    currentValue = value;
+
+    mainImage.src = x.target.src;
+
+    if (x.target.alt == "") {
+      modalTitle.innerHTML = "Agile Geek Images";
+    } else {
+      modalTitle.innerHTML = x.target.alt;
+    }
+  });
+});
+
+hiddenImgs.addEventListener("click", () => {
+  openImg.classList.remove("active");
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    openImg.classList.remove("active");
+  }
+});
